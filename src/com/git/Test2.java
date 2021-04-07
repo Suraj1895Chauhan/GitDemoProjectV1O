b5cbb0633528a52b2ab4ae94fb4907b0fedc1cca
Suraj Chauhan <suraj0001895@gmail.com>
2021-04-07 12:09:09 +0530

    commiting test 2 bramch2

---
+++ src/com/git/Test2.java
@@ -0,0 +1,20 @@
+package com.git;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("ear something");
+		}
+		else
+		{
+			System.out.println("do work");
+		}
+
+		System.out.println("do some modificiation..");
+	}
+
+}
